5252a08733b64614d5e49282cfe2dde5b5798b2e
mustafa648 <ly988835@gmail.com>
2026-09-03 20:15:39 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { NetworkDiagnostics } from '@/components/NetworkDiagnostics';
-import { InventoryPrediction } from '@/components/InventoryPrediction';
-import { CaptivePortalManager } from '@/components/CaptivePortalManager';
-import { DynamicPricingManager } from '@/components/DynamicPricingManager';
+import { NetworkDiagnostics } from '@/components/src/components/NetworkDiagnostics';
+import { InventoryPrediction } from '@/components/src/components/InventoryPrediction';
+import { CaptivePortalManager } from '@/components/src/components/CaptivePortalManager';
+import { DynamicPricingManager } from '@/components/src/components/DynamicPricingManager';
 
 export const Route = createFileRoute('/')({
   component: Index,
